7fd20c8baff6523825d0c60635b9c882a2b5e0b9
Cesar Carvajal <maguce3@hotmail.com>
2024-10-29 23:10:44 -0500

    implementandoInjeccionDependencia

--- lib/app/presentation/modules/splash/views/splash_view.dart
+++ lib/app/presentation/modules/splash/views/splash_view.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
+import '../../../../../main.dart';
+
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  Future<void> _init() async {
+   final connectivityRepository = Injector.of(context).connectivityRepository;
+   final hasInternet = await connectivityRepository.hasInternet;
+   if(hasInternet){
+   }else{ }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: 80,
+          height: 80,
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/material.dart';
 
+import 'app/data/repositories_implementation/authentication_repository_impl.dart';
+import 'app/data/repositories_implementation/connectivity_repository_impl.dart';
+import 'app/domain/repositories/authentication_repository.dart';
+import 'app/domain/repositories/connectivity_repository.dart';
 import 'app/my_app.dart';
 
 void main() {
-  runApp(
-   const MyApp(),
-  );
+  runApp(Injector(
+    connectivityRepository: ConnectivityRepositoryImpl(),
+    authenticationRepository: AuthenticationRepositoryImpl(),
+    child: const MyApp(),
+  ));
+}
+
+class Injector extends InheritedWidget {
+  const Injector({
+    super.key,
+    required super.child,
+    required this.connectivityRepository,
+    required this.authenticationRepository,
+  });
+
+  final ConnectivityRepository connectivityRepository;
+  final AuthenticationRepository authenticationRepository;
+
+  @override
+  // ignore: avoid_renaming_method_parameters
+  bool updateShouldNotify(_) => false;
+
+  static Injector of(BuildContext context) {
+    final injector = context.dependOnInheritedWidgetOfExactType<Injector>();
+    assert(injector != null, 'Injector could not be found');
+    return injector!;
+  }
 }
